d7908dcbf833af3b81828da8103b09715ecd825b
nimonht <trantatanthanh.cuchi@gmail.com>
2026-04-19 12:38:02 +0700

    Release v1.0.1-1

--- README.md
+++ README.md
@@ -232,7 +232,7 @@ scripts/generate-sample-data.py --help
 
 ## Version
 
-App version 1.0.1
+App version 1.0.1-1
 
 ## License
 
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.1-1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
